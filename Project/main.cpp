88152ddaf2e7111e5dfae7d40fdf12d960ac657b
kaloni <carl@dehlin.com>
2015-06-08 19:31:36 +0800

    Initial Commit

---
+++ Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project
+//
+//  Created by Carl Dehlin on 08/06/15.
+//  Copyright (c) 2015 Carl Dehlin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
