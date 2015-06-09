b49d4a5f40d8a9a7117fbb4e3d5dfcf9073fa7b0
kaloni <carl@dehlin.com>
2015-06-09 16:13:48 +0800

    Git tutorial

--- Project/main.cpp
+++ Project/main.cpp
@@ -5,6 +5,30 @@
 #include <vector>
 #include "HeapType.cpp"
 
+// "Git tutorial", Howver I am still not completely sure ~~
+/*
+ 0) Create a folder, name is something like "DataStruct..."
+    In the folder (in command/terminal "black box"), type
+    - git init
+    (I think you have to atleast)
+ 
+ 1) Get project
+    - git clone <url_name>
+ 
+ 2) Set remote
+    - git add remote origin <url_name>
+ 
+ 3) Do some thing. Then upload
+    - git add .
+    - git commit -m "message"
+    - git push origin master
+ 
+ 4) To get latest changes
+    - git fetch
+    - git merge origin/master
+ 
+ */
+
 using namespace std;
 
 enum StatusType {Free, Rented};
